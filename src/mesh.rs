c5f70072123f9debd26fdad78ae51fb3f87419cf
Thibaud Lambert <thibaud.lambert@gmail.com>
2015-12-27 17:45:12 +0100

    mesh : starts implementing add_face (not compiling)

--- src/mesh.rs
+++ src/mesh.rs
@@ -62,8 +62,67 @@ impl Mesh {
     pub fn n_vertices(& self) -> usize {
         self.n_v_
     }
-}
 
+    /// Returns if the Vertex is on a boundary
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use lwmesh::mesh::Mesh;
+    ///
+    /// let mut m = Mesh::new();
+    /// let v = m.add_vertex();
+    /// assert!(m.is_onboundary(v));
+    /// ```
+    pub fn is_boundary(&self, v : Vertex) {
+
+    }
+
+    /// Adds a new `Face` to the `Mesh`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use lwmesh::mesh::Mesh;
+    /// use lwmesh::handle::Vertex;
+    ///
+    /// let mut m = Mesh::new();
+    /// let mut vvec = Vec::<Vertex>::new();
+    /// for _ in 0..3 {
+    ///     vvec.push(m.add_vertex());
+    /// }
+    /// let f = m.add_face(&vvec);
+    /// ```
+    pub fn add_face(&mut self, vertices : & Vec<Vertex>) -> Face {
+        let n = vertices.len();
+        let mut hvec = Vec::<Halfedge>::new();
+        let mut new_hvec = Vec::<bool>::new();
+        hvec.reserve(n);
+        new_hvec.reserve(n);
+
+        // test for topological errors
+
+        for i in 0..n {
+            if !self.is_boundary(vertices[i]) {
+                return Face::invalid();
+            }
+            hvec[i] = self.find_halfedge(vertices[i],vertices[(i+1)%n]);
+            new_hvec[i] = !hvec[i].is_valid();
+            if !new_hvec[i] && !self.is_boundary(hvec) {
+                return Face::invalid();
+            }
+        }
+
+
+        // Creates the new face
+        let f = self.fconn_.push(FaceConnectivity::invalid());
+
+        // Inits the face
+        self.fconn_[f].halfedge_ = hvec[0];
+
+        return f;
+    }
+}
 
 #[cfg(test)]
 mod tests {
